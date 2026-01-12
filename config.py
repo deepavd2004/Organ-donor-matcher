3672eb711fabef5ee8340cd46def06570d4a5e01
deepavd2004 <02fe23bcs184@kletech.ac.in>
2026-01-12 19:48:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
-# config.py – simple config for Render demo (no real DB)
-
 import os
 
 # Secret key for sessions
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-# Dummy MySQL values – not actually used on Render in demo mode
-MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "shinkansen.proxy.rlwy.net")
 MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
-MYSQL_DB = os.environ.get("MYSQL_DB", "organ_donor_matcher")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "zonuwLbXLwFWiQTChisgQhPVDLWWEzON")
+MYSQL_DB = os.environ.get("MYSQL_DB", "railway")
