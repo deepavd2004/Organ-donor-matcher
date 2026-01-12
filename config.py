35d8e47b5015febfe2e0b45bf442570e45fa71bd
deepavd2004 <02fe23bcs184@kletech.ac.in>
2026-01-12 20:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-import os
-
-# Secret key for sessions
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-
-MYSQL_HOST = os.environ.get("MYSQL_HOST", "shinkansen.proxy.rlwy.net")
-MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "zonuwLbXLwFWiQTChisgQhPVDLWWEzON")
-MYSQL_DB = os.environ.get("MYSQL_DB", "railway")
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"              # or your MySQL user
+MYSQL_PASSWORD = "Root@1234" # same password you used in MySQL client
+MYSQL_DB = "organ_donor_matcher"
+SECRET_KEY = "some_random_secret_key"
